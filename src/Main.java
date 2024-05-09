6da39e92cec2d6aee4f328d1601fe351b9ec644a
AnCapone <okai.kot44@gmail.com>
2024-05-09 22:57:06 +0300

    Implement task 1

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,22 @@
+import java.util.List;
+import java.util.stream.Collectors;
+
 public class Main {
     public static void main(String[] args) {
 
-        System.out.println("Hello world!");
+        List<Product> products= List.of(
+            new Product("Vegetables", 100),
+            new Product("Book", 200),
+            new Product("Fruits", 0.80),
+            new Product("Toy", 0.50),
+            new Product("Book", 275),
+            new Product("Book", 251)
+        );
+
+        List<Product> books = products.stream()
+                .filter(product -> product.getType().equals("Book") && product.getPrice() > 250)
+                .toList();
+
+        System.out.println(books);
     }
 }
---
+++ src/Product.java
@@ -0,0 +1,25 @@
+public class Product {
+    private String type;
+    private double price;
+
+    public Product(String type, double price) {
+        this.type = type;
+        this.price = price;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+}
