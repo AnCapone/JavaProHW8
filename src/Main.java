b32bf46b8b4a82a14ac1543835daa29d06d52af2
AnCapone <okai.kot44@gmail.com>
2024-05-12 21:08:16 +0300

    Implement code compilable

--- src/Main.java
+++ src/Main.java
@@ -7,7 +7,21 @@ import java.util.stream.Collectors;
 
 public class Main {
     public static void main(String[] args) {
+    String productType = "Book";
+    double limitPrice = 250;
+    double discount = 0.9;
+    List<Product> products = List.of(
+        new Product("Book", 200),
+            new Product("Book", 300, LocalDate.of(2023, 2, 1)),
+            new Product("Book", 400, true, LocalDate.of(2021, 3, 1)),
+            new Product("Vegetable", 250, LocalDate.of(2022, 4, 1)),
+            new Product("Book", 600, true, LocalDate.of(2021, 5, 1)),
+            new Product("Toy", 0.9, false, LocalDate.of(2021, 6, 1)),
+            new Product("Vegetable", 800, true, LocalDate.of(2021, 7, 1)),
+            new Product("Book", 900, false, LocalDate.of(2021, 8, 1)),
+            new Product("Car", 1000, true, LocalDate.of(2021, 9, 1))
 
+    );
 
         List<Product> books = products.stream()
                 .filter(product -> product.getType().equals(productType) && product.getPrice() > limitPrice)
