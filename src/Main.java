c831d742b298ba93f4e12d0f444e8bd69b037eec
AnCapone <okai.kot44@gmail.com>
2024-05-12 17:54:43 +0300

    Implement Task 5

--- src/Main.java
+++ src/Main.java
@@ -7,16 +7,16 @@ import java.util.stream.Collectors;
 public class Main {
     public static void main(String[] args) {
         String productType = "Book";
-        Double limitPrice = 250.0;
+        Double limitPrice = 75.0;
         Double discount = 0.9;
 
         List<Product> products= List.of(
             new Product("Vegetables", 100),
-            new Product("Book", 200, true, LocalDate.of(2021, 1, 1)),
-            new Product("Book", 5.80, LocalDate.of(2021, 3, 1)),
+            new Product("Book", 70, true, LocalDate.of(2021, 1, 1)),
+            new Product("Book", 5.80, LocalDate.of(2024, 3, 1)),
             new Product("Toy", 0.50, LocalDate.of(2023, 5, 20)),
             new Product("Book", 275, true, LocalDate.of(2022, 10, 5)),
-            new Product("Book", 251, true, LocalDate.of(2023, 11, 30))
+            new Product("Book", 60, true, LocalDate.of(2024, 3, 30))
         );
 
         List<Product> books = products.stream()
@@ -37,5 +37,13 @@ public class Main {
                 .sorted(Comparator.comparing(Product::getDateAdded).reversed())
                 .limit(3)
                 .toList();
+
+        Double totalPriceThisYearBooks = products.stream()
+                .filter(product -> product.getType().equals(productType))
+                .filter(product -> product.getDateAdded().getYear() == LocalDate.now().getYear())
+                .filter(product -> product.getPrice() <= limitPrice)
+                .mapToDouble(Product::getPrice)
+                .sum();
     }
+
 }
