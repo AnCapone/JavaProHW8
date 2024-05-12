ba72cf157cdbbaac3a5985dfda6cee319634b46f
AnCapone <okai.kot44@gmail.com>
2024-05-12 17:34:12 +0300

    Implement Task 4

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,4 @@
+import java.time.LocalDate;
 import java.util.Comparator;
 import java.util.List;
 import java.util.NoSuchElementException;
@@ -11,11 +12,11 @@ public class Main {
 
         List<Product> products= List.of(
             new Product("Vegetables", 100),
-            new Product("Book", 200, true),
-            new Product("Book", 5.80),
-            new Product("Toy", 0.50),
-            new Product("Book", 275, true),
-            new Product("Book", 251, true)
+            new Product("Book", 200, true, LocalDate.of(2021, 1, 1)),
+            new Product("Book", 5.80, LocalDate.of(2021, 3, 1)),
+            new Product("Toy", 0.50, LocalDate.of(2023, 5, 20)),
+            new Product("Book", 275, true, LocalDate.of(2022, 10, 5)),
+            new Product("Book", 251, true, LocalDate.of(2023, 11, 30))
         );
 
         List<Product> books = products.stream()
@@ -32,6 +33,9 @@ public class Main {
                 .min(Comparator.comparing(Product::getPrice))
                 .orElseThrow(() -> new NoSuchElementException("Продукт [категорія: " + productType + "] не знайдено"));
 
-
+        List <Product> lastAddedProducts = products.stream()
+                .sorted(Comparator.comparing(Product::getDateAdded).reversed())
+                .limit(3)
+                .toList();
     }
 }
--- src/Product.java
+++ src/Product.java
@@ -1,17 +1,34 @@
+import java.time.LocalDate;
+import java.util.Date;
+
 public class Product {
     private String type;
     private double price;
     private boolean isDiscounted = false;
+    LocalDate dateAdded = LocalDate.now();
 
     public Product(String type, double price) {
         this.type = type;
         this.price = price;
+
     }
 
     public Product(String type, double price, boolean isDiscounted) {
         this.type = type;
         this.price = price;
         this.isDiscounted = isDiscounted;
+
+    }
+    public Product(String type, double price, boolean isDiscounted, LocalDate dateAdded) {
+         this.type = type;
+         this.price = price;
+         this.isDiscounted = isDiscounted;
+         this.dateAdded = dateAdded;
+    }
+    public Product(String type, double price, LocalDate dateAdded) {
+        this.type = type;
+        this.price = price;
+        this.dateAdded = dateAdded;
     }
 
     public String getType() {
@@ -36,5 +53,9 @@ public class Product {
 
     public boolean isDiscounted() {
         return isDiscounted;
+    }
+
+    public LocalDate getDateAdded() {
+        return dateAdded;
     }
 }
